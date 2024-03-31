b7262cf5402982436e3edb1e73aa551fe1ae27ce
Simen Daehlin <simen.daehlin@strapi.io>
2024-03-31 14:25:31 +0100

    Updated dockerfile

--- lib/generateDockerfile.ts
+++ lib/generateDockerfile.ts
@@ -1,53 +1,54 @@
+// Importing the necessary type for config parameter
 import { DockerFormState } from "@/lib/store/useFormStore";
 
+// Function to generate Dockerfile based on given configuration
 export const generateDockerfile = (config: DockerFormState): string => {
+  // Destructuring config to extract all necessary fields
   const {
     nodeVersion,
     env,
     packageManager,
-    buildStagePackages,
-    productionStagePackages,
-    user,
-    port,
+    user = "node",
+    port = 1337,
   } = config;
 
+  // Base commands for initial setup, applicable to both development and production environments
   const baseCommands = `FROM node:${nodeVersion}-alpine
-# Installing libvips-dev for sharp Compatibility
+# Installing libvips-dev for sharp compatibility and other dependencies
 RUN apk update && apk add --no-cache build-base gcc autoconf automake zlib-dev libpng-dev nasm bash vips-dev git
 ARG NODE_ENV=${env}
 ENV NODE_ENV=\${NODE_ENV}
+
 WORKDIR /opt/
-COPY package.json yarn.lock ./
+COPY package.json ${
+    packageManager === "yarn" ? "yarn.lock" : "package-lock.json"
+  } ./
 RUN ${
     packageManager === "yarn"
-      ? "yarn global add node-gyp"
-      : "npm install -g node-gyp"
-  }
-RUN ${packageManager} config set network-timeout 600000 -g && ${packageManager} install${
-    env === "production" ? " --production" : ""
-  }
+      ? "yarn config set network-timeout 600000 -g && yarn install"
+      : "npm config set fetch-retry-maxtimeout 600000 -g && npm install"
+  }${env === "production" ? " --production" : ""}
 ENV PATH /opt/node_modules/.bin:\$PATH
 WORKDIR /opt/app
 COPY . .
-RUN chown -R ${user || "node"}:${user || "node"} /opt/app
-USER ${user || "node"}`;
+RUN chown -R ${user}:${user} /opt/app
+USER ${user}`;
 
+  // Commands specific to development environment
   const devCommands = `
-RUN [${packageManager === "yarn" ? "yarn build" : "npm run build"}]
-EXPOSE ${port || 1337}
+EXPOSE ${port}
 CMD ["${packageManager}", "develop"]`;
 
+  // Commands for production environment, including multi-stage build
   const prodCommands = `
-RUN [${packageManager === "yarn" ? "yarn build" : "npm run build"}]
-
-# Creating final production image
-FROM node:${nodeVersion}-alpine
+FROM node:${nodeVersion}-alpine as build
 RUN apk add --no-cache vips-dev
-COPY --from=build /opt/node_modules ./node_modules
-COPY --from=build /opt/app ./
-RUN chown -R ${user || "node"}:${user || "node"} /opt/app
-EXPOSE ${port || 1337}
+COPY --from=0 /opt/node_modules ./node_modules
+COPY --from=0 /opt/app ./
+RUN chown -R ${user}:${user} /opt/app
+EXPOSE ${port}
 CMD ["${packageManager}", "start"]`;
 
+  // Returning the final Dockerfile content based on the environment
   return `${baseCommands}${env === "development" ? devCommands : prodCommands}`;
 };
